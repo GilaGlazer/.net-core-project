c35d6a9130437d6f6a44afe24b0d43a0f4f975c7
GilaGlazer <g0583247266@gmail.com>
2025-02-27 01:17:29 +0200

    lesson 3 18/02/2025

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
-
-namespace myProjectCore.Controllers;
+using webApiProject.Models;
+namespace webApiProject.Controllers;
 
 [ApiController]
 [Route("[controller]")]
@@ -11,22 +11,57 @@ public class WeatherForecastController : ControllerBase
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly List<WeatherForecast> listDays;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public WeatherForecastController()
     {
-        _logger = logger;
-    }
-
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
-    {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        listDays = Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             TemperatureC = Random.Shared.Next(-20, 55),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
         })
-        .ToArray();
+        .ToList(); ;
+
     }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return listDays;
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<WeatherForecast> Get(int id)
+    {
+        if (id < 0 || id > listDays.Count())
+            return BadRequest("Not Valid");
+        return listDays[id];
+    }
+
+    [HttpPost]
+    public void Post(WeatherForecast newItem)
+    {
+        listDays.Add(newItem);
+    }
+
+    [HttpPut("{id}")]
+    public ActionResult Put(int id, WeatherForecast newItem)
+    {
+        if (id < 0 || id > listDays.Count())
+            return BadRequest("Not Valid");
+        listDays[id] = newItem;
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult RuthDelete(int id)
+    {
+        if (id < 0 || id > listDays.Count())
+            return BadRequest("Not Valid"); ;
+        listDays.RemoveAt(id);
+        return NoContent();
+    }
+
+
 }
